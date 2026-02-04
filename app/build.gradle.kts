6236a4018a26219533d59cb004dc08a18ffa53db
sfsakhawat999 <sfsakhawat999@gmail.com>
2026-02-04 15:11:31 +0600

    chore: bump app version to 1.2.0 (code 10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.h2.wellspend"
         minSdk = 26
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.1.0"
+        versionCode = 10
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
