03df6600dd30375dc184ac0bbab96bdfc6e12663
sfsakhawat999 <sfsakhawat999@gmail.com>
2026-02-05 21:44:25 +0600

    chore: update app version to 1.2.2 (12)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.h2.wellspend"
         minSdk = 26
         targetSdk = 34
-        versionCode = 11
-        versionName = "1.2.1"
+        versionCode = 12
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
