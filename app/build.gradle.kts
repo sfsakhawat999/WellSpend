4a9b941cd8390b3bbb513da8a42ddd3023610bf9
sfsakhawat999 <sfsakhawat999@gmail.com>
2026-03-24 16:07:02 +0600

    Update app version to 1.2.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.h2.wellspend"
         minSdk = 26
         targetSdk = 34
-        versionCode = 14
-        versionName = "1.2.4"
+        versionCode = 15
+        versionName = "1.2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
