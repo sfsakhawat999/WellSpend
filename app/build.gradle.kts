804f60ecb823c44b98c100d8fffa4a151f8ffc46
sfsakhawat999 <sfsakhawat999@gmail.com>
2026-04-10 08:43:37 +0600

    chore: bump version code to 17 and version name to 1.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.h2.wellspend"
         minSdk = 26
         targetSdk = 34
-        versionCode = 16
-        versionName = "1.3.0"
+        versionCode = 17
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
